0bd074d4d7d90a9813ba629778eb11134c8aa37a
hibamahboob587 <58483393+hibamahboob587@users.noreply.github.com>
2025-06-22 20:00:04 +0500

    Update heart_monitor2.ino

--- heart_monitor2.ino
+++ heart_monitor2.ino
@@ -2,7 +2,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <MAX30105.h>
-#include "heartRate.h" // From SparkFun MAX3010x library
+#include "heartRate.h" 
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
